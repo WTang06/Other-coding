e46f973aa8e6cebaa1a0f344656eb543055f2bbe
WTang06 <33232639+WTang06@users.noreply.github.com>
2017-10-30 14:44:53 -0400

    Other codes I did besides challenges and Unity
    Other codes I did as a practice or a test

---
+++ VolumeCalculator(Improved)/VolumeCalculator(Improved)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace VolumeCalculator_Improved_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("box_data.txt");
+            StreamWriter writer = new StreamWriter("output.txt");
+
+            while (reader.Peek() != -1)
+            {
+                string line = reader.ReadLine();
+
+                string[] parts = line.Split(',');
+
+                int length = Convert.ToInt32(parts[1]);
+                int width = Convert.ToInt32(parts[2]);
+                int height = Convert.ToInt32(parts[3]);
+
+                int volume = length * width * height;
+
+                writer.WriteLine("The volume of the box is {0}", volume);
+
+            }
+
+            reader.Close();
+            writer.Close();
+        }
+    }
+}
